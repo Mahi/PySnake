e35cd554baa90404e02af941501d99bfe3217eae
Mahi <markusmeskanen@gmail.com>
2019-11-16 13:12:02 +0200

    Add docstrings for main module

--- snake/main.py
+++ snake/main.py
@@ -1,3 +1,18 @@
+"""The main module for executing the game.
+
+Takes care of starting the application and reading/writing highscores.
+The following command line arguments are required:
+
+- `--username`: Name of the player (for tracking high scores)
+- `--scorefile`: Path to the high score file
+
+One can optionally provide the following command line arguments too:
+
+- `-w` or `--width`: Width of the game screen (default: `480`)
+- `-h` or `--height`: Height of the game screen (default: `360`)
+- `--fps`: The framerate of the game, i.e. how often to update
+"""
+
 import argparse
 
 import ezpygame
@@ -6,6 +21,7 @@ import menu
 
 
 def parse_args():
+    """Parse the provided command line arguments."""
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('--username', required=True)
     parser.add_argument('--scorefile', required=True)
@@ -16,6 +32,14 @@ def parse_args():
 
 
 def read_high_scores(file_path):
+    """Read the previous high scores from a file.
+    
+    Prints out an issue if there are invalid lines or if the file
+    is missing altogether, but does not prevent the game from starting.
+
+    Be aware that the file will be overwritten with new high scores
+    as the game progresses.
+    """
     high_scores = []
     try:
         with open(file_path) as file:
@@ -38,6 +62,7 @@ def read_high_scores(file_path):
 
 
 def write_high_scores(file_path, high_scores):
+    """Write new high scores to a file."""
     with open(file_path, 'w') as file:
         for name, score in high_scores:
             file.write(f'{name}:{score}\n')
